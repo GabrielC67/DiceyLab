a0f10e6b01f50037134d943baadb06bba53a330c
GabrielC67 <gabecruzpro@gmail.com>
2025-05-26 23:43:36 -0400

    Checkpoint

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,41 @@
 
 public class Bins {
+    private int[] bins;
+    private int min; //For each number of Dice, there is a minimum value (sum)
+    private int max; //For each number of dice, there is a maximum value (sum)
 
+    //Constructor
+    public Bins(int min, int max){
+        this.min = min;
+        this.max = max;
+        this.bins = new int[max + 1];
+    }
+
+    //Boundaries are set through the if statement.
+    public void incrementBin(int value){
+        if(value >= min && value <= max){
+            bins[value]++;
+        }
+    }
+
+    //Boundaries are set through the if statement
+    public int getBin(int value){
+        if (value >= min && value <= max){
+            return bins[value];
+        }
+        return 0;
+    }
+
+
+    public int getMin(){
+        return min;
+    }
+
+    public int getMax(){
+        return max;
+    }
+
+    public int[] getBins(){
+        return bins;
+    }
 }
--- src/test/java/BinsTest.java
+++ src/test/java/BinsTest.java
@@ -1,4 +1,51 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BinsTest {
+
+    @Test
+    public void binsTest1(){
+        Bins results = new Bins(2, 12); //For a set of two dice
+        results.incrementBin(10);
+        results.incrementBin(10);
+        Integer numberOfTens = results.getBin(10);
+        assertEquals(2, numberOfTens);
+    }
+
+    @Test
+    public void binsTest2(){
+        Bins results = new Bins(5, 30); //For a set of five dice
+        results.incrementBin(30);
+        results.incrementBin(30);
+        results.incrementBin(30);
+        results.incrementBin(30);
+        Integer numberOfThirties = results.getBin(30);
+        assertEquals(4, numberOfThirties);
+    }
+
+    @Test
+    public void binsTest3(){
+        Bins results = new Bins(4, 24);
+        results.incrementBin(18);
+        results.incrementBin(18);
+        results.incrementBin(18);
+        results.incrementBin(18);
+        results.incrementBin(18);
+        Integer numberOfEighteens = results.getBin(18);
+        assertEquals(5, numberOfEighteens);
+    }
+
+    @Test
+    public void testOutOfBounds(){
+        Bins results = new Bins(2, 12); //Two dice
+
+        results.incrementBin(1); //Out of bounds using 1
+        Integer numberOfOnes = results.getBin(1);
+        assertEquals(0, numberOfOnes);
+
+        results.incrementBin(13); //Out of bounds using 13
+        Integer numberOfThirteens = results.getBin(13);
+        assertEquals(0, numberOfThirteens);
+    }
 }
